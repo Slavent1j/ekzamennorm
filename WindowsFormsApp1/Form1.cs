42b3b15834a3c12df8672a151b47665a14f948c7
Diveloment <91672317+Diveloment@users.noreply.github.com>
2022-12-09 12:26:17 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Слава удали коменты перед показом!!!!!!!!!!!!!!
+            // создание переменной с тектом из текстбокса
+            string myString = textBox1.Text.Trim();
+            // проверяем текст на полиндром
+            bool result = myString.SequenceEqual(myString.Reverse());
+            // выводим результат
+            if (result)
+                label1.Text = "Полиндром";
+            else
+                label1.Text = "Не полиндром";
+        }
+    }
+}
